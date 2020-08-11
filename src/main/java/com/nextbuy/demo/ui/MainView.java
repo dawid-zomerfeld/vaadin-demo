52b68715e9acbf095e520c6212d2e29e55200772
Dawid Zomerfeld <dawid.zomerfeld@optibuy.com>
2020-08-11 14:01:47 +0200

    part 10

--- src/main/java/com/nextbuy/demo/ui/MainView.java
+++ src/main/java/com/nextbuy/demo/ui/MainView.java
@@ -4,9 +4,11 @@ import com.nextbuy.demo.model.Company;
 import com.nextbuy.demo.model.Contact;
 import com.nextbuy.demo.service.CompanyService;
 import com.nextbuy.demo.service.ContactService;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -28,16 +30,38 @@ public class MainView extends VerticalLayout {
         addClassName("list-view");
         setSizeFull();
         configureGrid();
-        configureFilter();
+
 
         form = new ContactForm(companyService.findAll());
+        form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+        form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+        form.addListener(ContactForm.CloseEvent.class, e -> closeEditor());
 
         Div content = new Div(grid, form);
         content.addClassName("content");
         content.setSizeFull();
 
-        add(filterText, content);
+        add(getToolBar(), content);
         updateList();
+        closeEditor();
+    }
+
+    private void saveContact(ContactForm.SaveEvent event) {
+        contactService.save(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent event) {
+        contactService.delete(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void closeEditor() {
+        form.setContact(null);
+        form.setVisible(false);
+        removeClassName("editing");
     }
 
     private void configureGrid() {
@@ -51,13 +75,40 @@ public class MainView extends VerticalLayout {
         }).setHeader("Company");
 
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        grid.asSingleSelect().addValueChangeListener(event ->
+                editContact(event.getValue()));
     }
 
-    private void configureFilter() {
+    public void editContact(Contact contact) {
+        if (contact == null) {
+            closeEditor();
+        } else {
+            form.setContact(contact);
+            form.setVisible(true);
+            addClassName("editing");
+        }
+    }
+
+    private HorizontalLayout getToolBar() {
         filterText.setPlaceholder("Filter by name...");
         filterText.setClearButtonVisible(true);
         filterText.setValueChangeMode(ValueChangeMode.LAZY);
         filterText.addValueChangeListener(e -> updateList());
+
+        Button addContactButton = new Button("Add contact");
+        addContactButton.addClickListener(click -> addContact());
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
+
+
+
+    void addContact() {
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
     }
 
     private void updateList() {

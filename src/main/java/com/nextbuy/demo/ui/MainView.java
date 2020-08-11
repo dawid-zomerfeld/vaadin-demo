4cb005bca74dd250d7f86c12f4c479891cc0af24
Dawid Zomerfeld <dawid.zomerfeld@optibuy.com>
2020-08-11 12:30:32 +0200

    part 8

--- src/main/java/com/nextbuy/demo/ui/MainView.java
+++ src/main/java/com/nextbuy/demo/ui/MainView.java
@@ -1,24 +1,26 @@
 package com.nextbuy.demo.ui;
 
+import com.nextbuy.demo.model.Company;
 import com.nextbuy.demo.model.Contact;
 import com.nextbuy.demo.service.ContactService;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
 
 
 
 @Route("")
+@CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
 
+    private final ContactForm form;
     Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
+
     private ContactService contactService;
 
     public MainView(ContactService contactService) {
@@ -26,19 +28,40 @@ public class MainView extends VerticalLayout {
         addClassName("list-view");
         setSizeFull();
         configureGrid();
+        configureFilter();
 
-        add(grid);
+        form = new ContactForm();
+
+        Div content = new Div(grid, form);
+        content.addClassName("content");
+        content.setSizeFull();
+
+        add(filterText, content);
         updateList();
     }
 
     private void configureGrid() {
         grid.addClassName("contact-grid");
-
+        grid.setSizeFull();
         grid.removeColumnByKey("company");
         grid.setColumns("firstName", "lastName", "email", "status");
+        grid.addColumn(contact -> {
+            Company company = contact.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
+
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private void configureFilter() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
     }
 
     private void updateList() {
-        grid.setItems(contactService.findAll());
+        grid.setItems(contactService.findAll(filterText.getValue()));
     }
+
 }
